42c75f67da55ec1bba4db7dc7a3430942344ad1d
vandnagola <vandnagola756@gmail.com>
2024-01-29 23:33:45 +0530

    add one-to-many association b/w user and articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_25_172331) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_25_180430) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
