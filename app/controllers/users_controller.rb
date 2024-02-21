56509cb0dfe54fcf8ece5fe9dddc308b1e780c81
vandnagola <vandnagola756@gmail.com>
2024-02-22 00:12:42 +0530

    add account and all associated articles successfully deleted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-	before_action :set_user, only: %i[ show edit update ]
+	before_action :set_user, only: %i[ show edit update destroy]
 	before_action :require_user, only: [:edit, :update]
-  	before_action :require_same_user, only: [:edit, :update]
+  	before_action :require_same_user, only: [:edit, :update, :destroy]
 
 	def index
 		@users = User.paginate(page: params[:page], per_page:5)
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
 
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = user.id
 			flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
 			redirect_to articles_path
 		else
@@ -28,6 +29,13 @@ class UsersController < ApplicationController
 
 	def edit
     	
+  	end
+
+  	def destroy
+  		@user.destroy
+  		session[:user_id] = nil
+  		flash[:notice] = "Account and all associated articles successfully deleted"
+    	redirect_to articles_path
   	end
 
   	def update
