2dbe7034008d07e18733539ff7aec0be7ef09d08
vandnagola <vandnagola756@gmail.com>
2024-02-22 23:00:47 +0530

    add admin feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 	before_action :set_user, only: %i[ show edit update destroy]
 	before_action :require_user, only: [:edit, :update]
-  	before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
 	def index
 		@users = User.paginate(page: params[:page], per_page:5)
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
 
 		@user = User.new(user_params)
 		if @user.save
-			session[:user_id] = user.id
+			session[:user_id] = @user.id
 			flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
 			redirect_to articles_path
 		else
@@ -33,9 +33,9 @@ class UsersController < ApplicationController
 
   	def destroy
   		@user.destroy
-  		session[:user_id] = nil
+  		session[:user_id] = nil if @user == current_user
   		flash[:notice] = "Account and all associated articles successfully deleted"
-    	redirect_to articles_path
+    	redirect_to users_path
   	end
 
   	def update
@@ -59,7 +59,7 @@ class UsersController < ApplicationController
 	end
 
 	def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
       flash[:alert] = "You can only edit your own account"
       redirect_to @user
     end
