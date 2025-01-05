01cca12b54d58ad50cfbcc694c92a44bcb9211d7
vandnagola <vandnagola756@gmail.com>
2025-01-05 18:28:56 +0530

    fix javascript feature

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,5 +8,6 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
-pin "trix"
-pin "@rails/actiontext", to: "actiontext.js"
+
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
