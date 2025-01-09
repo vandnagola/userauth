8ff21611298edfb2df655bc3abc73e96398150da
vandnagola <vandnagola756@gmail.com>
2025-01-09 19:21:32 +0530

    modify ui alignment

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -4,13 +4,13 @@ module ArticlesHelper
 
     if article.file.content_type.start_with?('image/')
       safe_join([
-        link_to(image_tag(article.file, width: 540, height: 300), url_for(article.file)),
+        link_to(image_tag(article.file, width: 400, height: 300, class: 'card-img-top'), url_for(article.file)),
         content_tag(:p, link_to("Download File", download_article_path(article)))
       ])
     elsif article.file.content_type.start_with?('video/')
-      video_tag(url_for(article.file), controls: true, loop: true, width: 640, height: 360)
+      video_tag(url_for(article.file), controls: true, loop: true, width: 640, height: 360, class: 'card-img-top')
     elsif article.file.content_type.start_with?('audio/')
-      audio_tag(url_for(article.file), controls: true, muted: true)
+      audio_tag(url_for(article.file), controls: true, muted: true, class: 'card-img-top')
     else
       content_tag(:p, "Unsupported file type.")
     end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-	self.per_page = 10
+	self.per_page = 15
 	belongs_to :user
 	has_one_attached :file
 	validates :title, presence: true
